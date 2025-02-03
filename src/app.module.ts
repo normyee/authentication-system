d7fc06ccfa46e5dab4c2191e589a0ee43ee2a75c
normyee <lunoqro@gmail.com>
2025-02-03 12:43:44 -0300

    :building_construction: chore: organize architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './user-auth/adapters/app.controller';
+import { AppService } from './user-auth/application/app.service';
 
 @Module({
   imports: [],
