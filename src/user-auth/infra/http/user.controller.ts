4b2afbea84fa2fbc0ab18073942807911ecd73cb
normyee <lunoqro@gmail.com>
2025-02-04 18:39:06 -0300

    :recycle: refactor: user controller inject depedencies

---
+++ src/user-auth/infra/http/user.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/user-auth/infra/middlewares/guard/auth.guard';
+import { RegisterUserUseCase } from 'src/user-auth/application/usecases/register-user.use.case';
+import { LoginUserUseCase } from 'src/user-auth/application/usecases/login-user.use-case';
+import { LogoutUserUseCase } from 'src/user-auth/application/usecases/logout-user.use-case';
+import { RequestSession } from 'src/common/types';
+import { UserDTO } from 'src/user-auth/application/dtos/user.dto';
+import { LoginDTO } from 'src/user-auth/application/dtos/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly _registerUserUseCase: RegisterUserUseCase,
+    private readonly _loginUserUseCase: LoginUserUseCase,
+    private readonly _logoutUserUseCase: LogoutUserUseCase,
+  ) {}
+
+  @Post('signup')
+  async signUp(@Body() userDTO: UserDTO) {
+    return await this._registerUserUseCase.execute(userDTO);
+  }
+
+  @Post('login')
+  async login(@Body() credentials: LoginDTO) {
+    return await this._loginUserUseCase.execute(credentials);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('logout')
+  async logout(@Req() req: RequestSession) {
+    const sessionToken = req.session.token;
+    return await this._logoutUserUseCase.execute(sessionToken);
+  }
+}
