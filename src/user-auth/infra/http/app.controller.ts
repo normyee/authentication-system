22a67bf6a3bf7f3116ca92ad19d68e6d7dd37546
normyee <lunoqro@gmail.com>
2025-02-03 22:58:35 -0300

    :poop: feat: create list (WIP)

--- src/user-auth/infra/http/app.controller.ts
+++ src/user-auth/infra/http/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from '../../application/app.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
-@Controller()
+@UseGuards(AuthGuard)
+@Controller('list')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello(): Promise<any> {
-    return this.appService.getHello();
+    return this.appService.create();
   }
 }
