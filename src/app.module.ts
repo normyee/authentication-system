001b901c4c4953129812b34b9b687697544ec518
normyee <lunoqro@gmail.com>
2025-02-04 18:46:35 -0300

    :broom: chore: fix importings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './user-auth/infra/http/list.controller';
 import { PrismaModule } from './user-auth/infra/database/prisma/prisma.module';
 import { UserRepository } from './user-auth/infra/database/prisma/repositories/user.repository';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './user-auth/infra/modules/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ListRepository } from './user-auth/infra/database/prisma/repositories/list.repository';
-import { RedisModule } from './user-auth/infra/database/redis/redis.module';
 import { RedisService } from './user-auth/infra/database/redis/redis.service';
 import { CreateListUseCase } from './user-auth/application/usecases/create-list.use-case';
 import { UpdateListUseCase } from './user-auth/application/usecases/update-list.use-case';
 import { GetListByIdUseCase } from './user-auth/application/usecases/get-list-by-id.use-case';
 import { DeleteListUseCase } from './user-auth/application/usecases/delete-list.use-case';
 import { jwtSecurity } from './user-auth/infra/services/jwt-security.service';
+import { MemoryCacheModule } from './user-auth/infra/modules/memory-cache.module';
 
 @Module({
   imports: [
     JwtModule.register({ global: true, secret: 'jh;H=[}GsYn0rPd7H->H' }),
     PrismaModule,
     AuthModule,
-    RedisModule,
+    MemoryCacheModule,
   ],
   controllers: [AppController],
   providers: [
