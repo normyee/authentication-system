2ec48ff6ea1f6ad314f898fd4ef13ca7941a7c19
normyee <lunoqro@gmail.com>
2025-02-03 23:00:23 -0300

    :broom: chore: remove code lines

--- src/user-auth/application/app.service.ts
+++ src/user-auth/application/app.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from '../infra/database/prisma/repositories/user.repository';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly userRepository: UserRepository) {}
-  async getHello(): Promise<any> {
-    await this.userRepository.create({
-      name: 'fabricia',
-      email: 'fabricia@gmail.com',
-      password: 'dajsidjasidjasid',
-    });
+  create(): string {
     return 'Hello World!';
   }
 }
